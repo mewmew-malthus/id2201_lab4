48f5401f7163b8fde4119058a3ada7267ba90935
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-09-19 14:02:04 +0200

    case 3 solved

--- Readme.md
+++ Readme.md
@@ -5,3 +5,12 @@ First, I notice that in section 3 the document says we do not need to report whe
 
 Ok, so it was intentionally misleading. Multicast recovery is not supposed to work. Why?
 The election requires that all workers have the same view, and that the view is accurate. The most likely way this fails is a worker crashes, then when the election happens, the crashed leader is elected. Of course, that case is not exposed in our lab. Really, the color is dependent on all of the previous messages, and some panels do not receive messages during a crash.
+
+
+Things to handle:
+1. Lost Messages
+2. Failure Detector does not work
+3. Incorrect Messages
+
+## Lost Messages:
+BEAM guarantees the messages are delivered if FIFO order. Because each message already has an order number, it is trivial to detect a message has been missed - the N value attached to the message will increment by more than 1. In that case, the process requests state from the Leader, and updates its state to match.
---
+++ gms4.erl
@@ -0,0 +1,122 @@
+-module(gms3).
+-export([start/1, start/2]).
+
+bcast(Id, Msg, Slaves) ->
+    % io:format("Leader ~w Broadcasting: ~w ~n", [Id, Msg]),
+    lists:foreach(fun(Slave) -> 
+        Slave ! Msg, 
+        crash(Id) 
+    end, Slaves).
+
+crash(Id) ->
+    case random:uniform(42) of
+        42 ->
+            io:format("leader ~w: crash~n", [Id]),
+            exit(no_luck);
+        _ ->
+            ok
+    end.
+
+leader(Id, Master, Slaves, Group, N) ->
+    receive
+        {mcast, Msg} ->
+            bcast(Id, {msg, N, Msg}, Slaves),
+            Master ! Msg,
+            leader(Id, Master, Slaves, Group, N+1);
+        {join, Wrk, Peer} ->
+            Slaves2 = lists:append(Slaves, [Peer]),
+            Group2 = lists:append(Group, [Wrk]),
+            bcast(Id, {view, N, [self()|Slaves2], Group2}, Slaves2),
+            Master ! {view, Group2},
+            leader(Id, Master, Slaves2, Group2, N+1);
+        stop ->
+            ok
+    end.
+
+miss_msg(Chance, Message) ->
+    case random:uniform(Chance) of
+        Chance ->
+            miss;
+        _ -> 
+            Message
+    end.
+
+slave(Id, Master, Leader, Slaves, Group, N, Last) ->
+    receive
+        {'DOWN', _Ref, process, Leader, _Reason} ->
+            election(Id, Master, Slaves, Group, N, Last);
+        {mcast, Msg} ->
+            Leader ! {mcast, Msg},
+            slave(Id, Master, Leader, Slaves, Group, N, Last);
+        {join, Wrk, Peer} ->
+            Leader ! {join, Wrk, Peer},
+            slave(Id, Master, Leader, Slaves, Group, N, Last);
+        {msg, K, _Msg} when K > (N + 1) ->
+            %missed message
+            io:format("Node ~w Missed Message~n", [Id]);
+        {view, K, _nodes, _group} when K > (N + 1) ->
+            % missed message
+            io:format("Node ~w missed message~n", [Id]);
+        {msg, K, Msg} when K == (N + 1) ->
+            Master ! Msg,
+            slave(Id, Master, Leader, Slaves, Group, K, {msg, K, Msg});
+        {view, K, [Leader|Slaves2], Group2} when K == (N + 1) ->
+            Master ! {view, Group2},
+            slave(Id, Master, Leader, Slaves2, Group2, K, {view, K, [Leader|Slaves2], Group2});
+        {msg, K, _} when K =< N ->
+            io:format("Node ~w received old msg num: ~w~n", [Id, K]),
+            slave(Id, Master, Leader, Slaves, Group, N, Last);
+        {view, K, _, _} when K =< N ->
+            slave(Id, Master, Leader, Slaves, Group, N, Last);
+        stop ->
+            ok;
+        miss ->
+            slave(Id, Master, Leader, Slaves, Group, N, Last);
+        Badmessage ->
+            io:format("Bad message received ~w~n", [Badmessage])
+    end.
+
+election(Id, Master, Slaves, [_|Group], N, Last) ->
+    Self = self(),
+    case Slaves of
+        [Self|Rest] ->
+            bcast(Id, Last, Rest),
+            bcast(Id, {view, N+1, Slaves, Group}, Rest),
+            Master ! {view, Group},
+            leader(Id, Master, Rest, Group, N+2);
+        [Leader|Rest] ->
+            erlang:monitor(process, Leader),
+            slave(Id, Master, Leader, Rest, Group, N, Last)
+    end.
+
+% master start and init
+start(Id) ->
+    Self = self(),
+    Rnd = random:uniform(1000),
+    {ok, spawn_link(fun()-> init(Id, Self, Rnd) end)}.
+
+init(Id, Master, Rnd) ->
+    io:format("random val is ~w~n", [Rnd]),
+    random:seed(Rnd, Rnd, Rnd),
+    leader(Id, Master, [], [Master], 0).
+
+% slave start and init
+start(Id, Grp) ->
+    Self = self(),
+    Rnd = random:uniform(1000),
+    {ok, spawn_link(fun()-> init(Id, Grp, Self, Rnd) end)}.
+
+init(Id, Grp, Master, Rnd) ->
+    io:format("random val is ~w~n", [Rnd]),
+    random:seed(Rnd, Rnd, Rnd),
+    Self = self(),
+    Grp ! {join, Master, Self},
+    receive
+        {view, N, [Leader|Slaves], Group} ->
+        Master ! {view, Group},
+        erlang:monitor(process, Leader),
+        slave(Id, Master, Leader, Slaves, Group, N, {})
+    after 5000 ->
+        % ok
+        Master ! {error, "no reply from leader"}
+    end.
